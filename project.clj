546c15d01a64ce06843cc46a929ca73ec36c522a
Daniel Dyba <daniel.dyba@gmail.com>
2013-05-27 08:47:16 -0700

    Added :main to project file
    * Now you can run 'lein run' to get the server running
    * Modified the README file extension and added additional documentation

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
 		 [compojure "0.6.0-RC1"]
                  [ring "0.3.6"]
 		 [ring/ring-jetty-adapter "0.3.6"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :main clj-rest-svc.core)
 ;  :dev-dependencies [[swank-clojure "1.2.1"]])
